3ee296c4d67a95f97d0b93f6ca02eaadc7a57a05
VadymPenyaka <112482661+VadymPenyaka@users.noreply.github.com>
2023-07-18 22:29:35 +0300

    delete anum for Curencies Add getRatesHttpGetRequset to CurencyService, and make it to return hashmap with name of currency and its rate from api

--- src/main/java/org/projects/Main.java
+++ src/main/java/org/projects/Main.java
@@ -1,7 +1,25 @@
 package org.projects;
 
+import java.io.IOException;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        CurencyService curencyService = new CurencyService();
+        Map<String, Double> ratesMap;
+        try {
+            ratesMap = curencyService.getRatesHttpGetRequset();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (String nameOfCurency:ratesMap.keySet()) {
+            if (nameOfCurency.equals("USD")) {
+                System.out.println(ratesMap.get(nameOfCurency));
+            }
+        }
     }
+
+
 }
