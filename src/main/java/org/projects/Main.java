a9d2a49d4e5519514e61355bd0412ab1a2ad5c65
VadymPenyaka <112482661+VadymPenyaka@users.noreply.github.com>
2023-07-21 00:49:07 +0300

    add interface add enterAmount, enterCurrency and perform to Main class

--- src/main/java/org/projects/Main.java
+++ src/main/java/org/projects/Main.java
@@ -1,6 +1,7 @@
 package org.projects;
 
 import org.projects.currency.CurencyService;
+import org.projects.currency.CurrencyData;
 import org.projects.exception.CustomException;
 
 import java.io.IOException;
@@ -11,34 +12,54 @@ public class Main {
     private static CurencyService curencyService = new CurencyService();
     public static void main(String[] args) throws IOException, CustomException {
         System.out.println("Welcome to currency converter!");
-        System.out.println("Enter curency:");
-
         curencyService.getRatesHttpGetRequset();
-
-
+        perform();
     }
 
-    private static double choseAndPerformAction (double amount, String currencyName) {
+    private static double choseAndPerformExchange (double amount, CurrencyData currencyData) {
         System.out.println("Chose сonvert currency to UAH(enter 1) or UAH to currency(enter any other number):");
         int action = scanner.nextInt();
 
         switch (action) {
             case 1 -> {
-                try {
-                    return curencyService.convertToUAH(amount, curencyService.getCurrencyDateByName(currencyName));
-                } catch (CustomException e) {
-                    throw new RuntimeException(e);
-                }
+                    return curencyService.convertToUAH(amount, currencyData);
             }
             default -> {
-                try {
-                    return curencyService.convertUAHToCurrency(amount, curencyService.getCurrencyDateByName(currencyName));
-                } catch (CustomException e) {
-                    throw new RuntimeException(e);
-                }
+                    return curencyService.convertUAHToCurrency(amount, currencyData);
             }
         }
 
+    }
+
+    private static void perform () {
+        System.out.println("Resullt: "+ choseAndPerformExchange(enterAmount(), enterCurrency()));
+        System.out.println("If you wont to continue press 1, or enter any other number if you wont ot exit");
+        if (scanner.nextInt()==1)
+            perform();
+    }
+
+    private static CurrencyData enterCurrency () {//make method return exception message
+        CurrencyData currencyData;
+        System.out.println("Enter curency:");
+//        scanner.nextLine();
+        String name = scanner.nextLine();
+        try {
+            currencyData= curencyService.getCurrencyDateByName(name);
+        } catch (CustomException e) {
+            currencyData=enterCurrency();
+        }
+        return currencyData;
+    }
+
+    private static double enterAmount () {
+        System.out.println("Enter amount:");
+        double amount = scanner.nextDouble();
+        scanner.nextLine();
+        if (amount<=0) {
+            System.err.println("Amount must be greater than zero!");
+            return enterAmount();
+        }
+        return amount;
     }
 
 
